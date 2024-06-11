a435dc15a98bd66b3988956d083a08e3dc5a137d
natzxn <skrzypskanatalia@gmail.com>
2024-06-11 18:34:12 +0200

    Add calendar validation, castle chose option

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/reservation.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { Input, Select } from "antd";
-import DateRangeSelector from './Calendar';
-import 'react-date-range/dist/styles.css';
+import DateRangeSelector from './Form/Calendar';
+import 'react-date-range/dist/styles.css'; 
 import 'react-date-range/dist/theme/default.css';
 import { Checkbox } from 'antd';
 import type { CheckboxProps } from 'antd';
 
+import FirstCastle from '../assets/castle3.png'
+import SecondCastle from '../assets/castle2.png'
+import ThirdCastle from '../assets/castle4.png'
+
 const { Option } = Select;
 
+type CastleOption = 'first-option' | 'second-option' | 'third-option';
+
 export const Reservation = () => {
+  const [selectedCastle, setSelectedCastle] = useState<CastleOption | null>(null);
+
   const validationSchema = Yup.object({
     name: Yup.string().required('Imię i Nazwisko są wymagane'),
     phoneNumber: Yup.string().required('Numer Telefonu jest wymagany'),
@@ -25,31 +33,58 @@ export const Reservation = () => {
     console.log('Wartości formularza:', values);
   };
 
-  const handleDateChange = (ranges: { startDate: Date | null, endDate: Date | null }) => {
-    console.log('Zakres dat:', ranges);
+  const handleDateChange = (ranges: { startDate: Date | null, endDate: Date | null }, setFieldValue: (field: string, value: any) => void) => {
+    setFieldValue('startDate', ranges.startDate);
+    setFieldValue('endDate', ranges.endDate);
   };
+
 
   const onChange: CheckboxProps['onChange'] = (e) => {
     console.log(`checked = ${e.target.checked}`);
   };
 
+  const handleCastleChange = (value: CastleOption) => {
+    setSelectedCastle(value);
+  };
+
+  const renderCastleImage = () => {
+    switch (selectedCastle) {
+      case "first-option":
+        return <img src={FirstCastle} alt="First Castle" />;
+      case "second-option":
+        return <img src={SecondCastle} alt="Second Castle" />;
+      case "third-option":
+        return <img src={ThirdCastle} alt="Third Castle" />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <section className="reservation">
-      <div className="container mt-10 mx-auto max-w-7xl flex">
-        <div className="form-container mx-8">
+      <div className="container mt-10 max-w-7xl flex">
+        <div className="form-container mx-14">
           <h1 className="heading-reservation mb-5">Rezerwacja</h1>
           <Formik
             initialValues={{
               name: "",
               phoneNumber: "",
-              castle: "",
+              castle: "" as CastleOption | "",
               delivery: "",
               payment: "",
+              checkbox: false,
             }}
             validationSchema={validationSchema}
             onSubmit={onSubmit}
           >
-            {({ values, handleChange, handleBlur }) => (
+            {({
+              values,
+              handleChange,
+              handleBlur,
+              touched,
+              errors,
+              setFieldValue,
+            }) => (
               <Form>
                 <div className="form-group mb-4">
                   <Input
@@ -89,7 +124,8 @@ export const Reservation = () => {
                     placeholder="Wybierz rodzaj dmuchanej atrakcji"
                     value={values.castle || undefined}
                     onChange={(value) => {
-                      handleChange("castle")(value);
+                      setFieldValue("castle", value);
+                      handleCastleChange(value as CastleOption);
                     }}
                     onBlur={handleBlur}
                   >
@@ -113,10 +149,18 @@ export const Reservation = () => {
                   <h4 className="choose-day mb-4">
                     Wybierz dzień dostawy i odbioru:
                   </h4>
+                  <div className='flex'>
                   <DateRangeSelector
-                    onDateChange={handleDateChange}
+                    onDateChange={(ranges) => handleDateChange(ranges, setFieldValue)}
                     disabled={!values.castle}
                   />
+                  <h5 className='ml-8 max-w-44 my-20 text-sm'>
+                    Dni zaznaczone na szaro (te których nie da sie wybrać)
+                    oznaczają że dana atrakcja w tym terminie jest nie dostępna.
+                    <br/><br/>Możesz spróbować wybrać innego dmuchańca i sprawdzić
+                    dostępność w kalendarzu.
+                  </h5>
+                  </div>
                 </div>
                 <div className="form-group mb-4">
                   <Select
@@ -124,14 +168,12 @@ export const Reservation = () => {
                     placeholder="Wybierz rodzaj dostawy"
                     value={values.delivery || undefined}
                     onChange={(value) => {
-                      handleChange("delivery")(value);
+                      setFieldValue("delivery", value);
                     }}
                     onBlur={handleBlur}
                   >
-                    <Option value="first-option">Odbiór osobisty</Option>
-                    <Option value="second-option">
-                      Dostawa na wybrany adres
-                    </Option>
+                    <Option value="personal-collect">Odbiór osobisty</Option>
+                    <Option value="home-delivery">Dostawa na wybrany adres</Option>
                   </Select>
                   <ErrorMessage
                     name="delivery"
@@ -145,13 +187,13 @@ export const Reservation = () => {
                     placeholder="Wybierz rodzaj płatności przy odbiorze"
                     value={values.payment || undefined}
                     onChange={(value) => {
-                      handleChange("payment")(value);
+                      setFieldValue("payment", value);
                     }}
                     onBlur={handleBlur}
                   >
-                    <Option value="first-option">Gotówka</Option>
-                    <Option value="second-option">Karta płatnicza</Option>
-                    <Option value="third-option">BLIK</Option>
+                    <Option value="cash">Gotówka</Option>
+                    <Option value="card">Karta płatnicza</Option>
+                    <Option value="blik">BLIK</Option>
                   </Select>
                   <ErrorMessage
                     name="payment"
@@ -159,39 +201,49 @@ export const Reservation = () => {
                     className="error-message"
                   />
                 </div>
-                <div className="checkbox whitespace-nowrap max-w-xl mt-8 mb-14 flex">
-                  <Checkbox onChange={onChange}>
-                    <p className="text-lg ">
-                      Wyrażam zgode na przetwarzanie moich danych osobowych w
+                <div className="checkbox-wrapper xl:whitespace-normal max-w-lg mt-8 mb-14">
+                  <Checkbox
+                    name="checkbox"
+                    className={`ant-checkbox ${
+                      touched.checkbox && errors.checkbox
+                        ? "ant-checkbox-error"
+                        : ""
+                    }`}
+                    checked={values.checkbox}
+                    onChange={(e) => {
+                      setFieldValue("checkbox", e.target.checked);
+                      onChange(e);
+                    }}
+                  >
+                    <p className="text-lg">
+                      Wyrażam zgodę na przetwarzanie moich danych osobowych w
                       celu i zakresie koniecznym do udzielenie odpowiedzi na
                       przesłane zapytanie.
                     </p>
                   </Checkbox>
                 </div>
-                <button
-                  type="submit"
-                  className="btn-submit btn-primary mb-14"
-                  onClick={onSubmit}
-                >
+                <button type="submit" className="btn-submit btn-primary mb-14">
                   REZERWUJ
                 </button>
               </Form>
             )}
           </Formik>
         </div>
-        <div className="bubbles-container mr-10 mt-14">
+        <div className="bubbles-container max-w-xs mr-10 mt-14">
           <figure className="bubbles-images mb-28">
             <div className="cup cup1 smaller"></div>
             <div className="cup cup1 larger"></div>
-            <div className="chosen-castle"></div>
+            <div className="chosen-castle">{renderCastleImage()}</div>
           </figure>
           <figure className="bubbles-images">
             <div className="cup cup2 smaller2"></div>
             <div className="cup cup2 larger2"></div>
-            <div className="chosen-castle"></div>
           </figure>
         </div>
       </div>
+      <aside className='mt-8 mb-6 lg:mx-14 xl:mx-16'>
+        <p className='text-cyan-700'>Lorem ipsum dolor sit amet consectetur. Pretium eget aliquam praesent vitae mauris pulvinar quam ultricies.</p>
+      </aside>
     </section>
   );
 };
