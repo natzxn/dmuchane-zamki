bc468f72fd52b926c6a98c2d9d1bbe4505e23c5f
natzxn <skrzypskanatalia@gmail.com>
2024-06-12 18:01:11 +0200

    Adding validation to delivery fields

--- src/components/Form/Delivery.tsx
+++ src/components/Form/Delivery.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Select, Input, TimePicker } from 'antd';
 import dayjs from 'dayjs';
+import { ErrorMessage, useFormikContext } from 'formik';
 import '../../styles/delivery.css'
 
 const { Option } = Select;
@@ -13,6 +14,7 @@ interface DeliveryOptionProps {
 
 const DeliveryOption: React.FC<DeliveryOptionProps> = ({ value, onChange }) => {
     const [showAddressFields, setShowAddressFields] = useState(false);
+    const { setFieldValue } = useFormikContext();
 
     const handleDeliveryChange = (value: string) => {
         onChange(value);
@@ -32,37 +34,108 @@ const DeliveryOption: React.FC<DeliveryOptionProps> = ({ value, onChange }) => {
           </Select>
           {showAddressFields && (
             <>
-              <div className="adress-inputs mt-4 mb-4">
-                <Input
+              <div className="adress-inputs flex mt-4 mb-4">
+                <div className="form-group">
+                  <Input
                     type="text"
-                  placeholder="Miejscowość"
-                  className="address-field mr-8"
-                  name="city"
-                />
-                <Input type="text" placeholder="Ulica" className="address-field" name="street"/>
+                    placeholder="Miejscowość"
+                    className="address-field mr-8"
+                    name="addressCity"
+                    onChange={(e) => {
+                      setFieldValue("addressCity", e.target.value); 
+                    }}
+                  />
+                  <ErrorMessage
+                    name="addressCity"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+                <div className="form-group">
+                  <Input
+                    type="text"
+                    placeholder="Ulica"
+                    className="address-field"
+                    name="addressStreet"
+                    onChange={(e) => {
+                      setFieldValue('addressStreet', e.target.value); 
+                    }}
+                  />
+                  <ErrorMessage
+                    name="addressStreet"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
               </div>
-              <div className="adress-inputs mb-4">
-                <Input
-                type="number"
-                  placeholder="Numer domu"
-                  className="address-field mr-8"
-                  name="home-number"
-                />
-                <Input type="text" pattern="[0-9]{2}-[0-9]{3}" placeholder="Kod pocztowy" className="address-field" name="zip-code"/>
+              <div className="adress-inputs flex mb-4">
+                <div className="form-group">
+                  <Input
+                    type="text"
+                    placeholder="Numer domu/lokalu"
+                    className="address-field mr-8"
+                    name="addressHomeNumber"
+                    onChange={(e) => {
+                      setFieldValue('addressHomeNumber', e.target.value); 
+                    }}
+                  />
+                  <ErrorMessage
+                    name="addressHomeNumber"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+                <div className="form-group">
+                  <Input
+                    type="text"
+                    pattern="[0-9]{2}-[0-9]{3}"
+                    placeholder="Kod pocztowy"
+                    className="address-field"
+                    name="addressZipCode"
+                    onChange={(e) => {
+                      setFieldValue('addressZipCode', e.target.value); 
+                    }}
+                  />
+                  <ErrorMessage
+                    name="addressZipCode"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
               </div>
               <div className="time-pickers flex mb-4">
                 <div className="time-field mr-8">
                   <h5 className="mb-1">Preferowana godzina dostawy:</h5>
                   <TimePicker
-                    defaultValue={dayjs("12:00", format)}
+                    defaultValue={null}
                     format={format}
+                    name="deliveryTime"
+                    onChange={(time) => {
+                      const timestamp = time.valueOf();
+                      setFieldValue('deliveryTime', timestamp);
+                    }}
+                  />
+                    <ErrorMessage
+                    name="deliveryTime"
+                    component="div"
+                    className="error-message"
                   />
                 </div>
-                <div className='time-field'>
+                <div className="time-field">
                   <h5 className="mb-1">Preferowana godzina odbioru:</h5>
                   <TimePicker
-                    defaultValue={dayjs("12:00", format)}
+                    defaultValue={null}
                     format={format}
+                    name="pickUpTime"
+                    onChange={(time) => {
+                      const timestamp = time.valueOf();
+                      setFieldValue('pickUpTime', timestamp);
+                    }}
+                  />
+                    <ErrorMessage
+                    name="pickUpTime"
+                    component="div"
+                    className="error-message"
                   />
                 </div>
               </div>
